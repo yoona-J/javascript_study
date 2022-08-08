1b32c9331502590e007530575e41e320c2e0a651
yoona_J <yoonalove77@naver.com>
2022-08-08 15:49:24 +0900

    Delete 8-4.js

--- 8-4.js
+++
@@ -1,9 +0,0 @@
-//하노이의 탑
-
-function hanoi(n, a, b, c) {
-    if (n < 1) { return }
-    hanoi(n-1, a, c, b);
-    console.log(n + " 번째 원반 : " + a + " -> " + c);
-    hanoi(n-1, b, a, c);
-}
-hanoi(4, "A", "B", "C")
